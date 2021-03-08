f9edac63f4152f521dc9edb69c90a458e0485e9e
Ocione Junior <ocionejunior@gmail.com>
2021-03-08 16:58:01 -0300

    Listagem de postagens

---
+++ app.js
@@ -0,0 +1,91 @@
+// Carregando módulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const admin = require('./routes/admin')
+const path = require('path')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
+
+// Configurações
+    // Sessão
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    // Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash('error_msg')
+        next()
+    })
+
+    // Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    //Handlebars
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    // Public
+    app.use(express.static(path.join(__dirname, "public")))
+
+    app.use((req, res, next) =>{
+        console.log('Oi eu sou um middleware')
+        next()
+    })
+
+    // Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/blogapp', {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    }).then(() => {
+        console.log('Conectado ao mongo')
+    }).catch((err) => {
+        console.log('Erro ao se conectar: ' + err)
+    })
+
+// Rotas
+app.get('/', (req, res) => {
+    Postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens) => {
+        res.render("index", {postagens: postagens})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/404")
+    })
+})
+
+app.get("/postagem/:slug", (req, res) => {
+    Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
+        if(postagem){
+            res.render("postagem/index", {postagem: postagem})
+        }else{
+            req.flash("error_msg", "Esta postagem não existe")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/")
+    })
+})
+
+app.get("/404", (req, res) => {
+    res.send("Erro 404")
+})
+
+app.use('/admin', admin)
+
+// Outros
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log('Servidor rodando!')
+})
