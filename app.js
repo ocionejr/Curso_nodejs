1bc043e37d0c9f31a655b626b75bdbf7932b104c
Ocione Junior <ocionejunior@gmail.com>
2021-03-17 17:42:18 -0300

    Formulário de usuário

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require("./models/Postagem")
 const Postagem = mongoose.model("postagens")
 require("./models/Categoria")
 const Categoria = mongoose.model("categorias")
+const usuarios = require('./routes/usuario') 
 
 // Configurações
 // Sessão
@@ -117,6 +118,7 @@ app.get("/404", (req, res) => {
 })
 
 app.use('/admin', admin)
+app.use('/usuarios', usuarios)
 
 // Outros
 const PORT = 8081
